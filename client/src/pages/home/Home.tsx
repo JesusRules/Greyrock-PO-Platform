4cf41357ef37f70f6c7b62bc50f2fba3cd748ec8
Justin John Bernard <justin.bernard320@gmail.com>
2025-05-08 12:04:14 -0300

    Hope this works

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,10 +1,14 @@
 import { PurchaseOrderList } from "../../components/purchase-order/po-list";
+import { Button } from "../../components/ui/button"
 
 export default function Home() {
   return (
-    <div className="container mx-auto py-10">
+    <div className="mx-auto py-10 mt-5 max-w-[1100px]">
       <h1 className="text-3xl font-bold mb-6">Purchase Order Management</h1>
         <PurchaseOrderList />
+        <Button className="ml-auto flex mt-2" color="blue" onClick={() => console.log("Logout")}>
+          Logout
+        </Button>
     </div>
   )
 }
